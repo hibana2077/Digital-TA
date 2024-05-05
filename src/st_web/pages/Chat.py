79f158e077d6558707b7ac6b47f1ab23e3e64c78
hibana2077 <hibana2077@gmail.com>
2024-05-05 17:42:47 +0800

    chore: Add import requests to Chat.py

--- src/st_web/pages/Chat.py
+++ src/st_web/pages/Chat.py
@@ -2,11 +2,12 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-05-05 14:01:07
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-05-05 15:05:23
+LastEditTime: 2024-05-05 17:42:42
 FilePath: \Digital-TA\src\st_web\pages\Chat.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import streamlit as st
+import requests
 
 st.header("Chat")
 
