8f644d2c0a5196c095da32b17c2eb0016ec3a86a
hibana2077 <hibana2077@gmail.com>
2024-05-07 00:02:54 +0800

    chore: Add endpoint to retrieve file count in test_file_count function

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-05-06 21:09:40
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-05-06 23:38:46
+LastEditTime: 2024-05-06 23:55:55
 FilePath: \Digital-TA\src\backend\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -38,6 +38,15 @@ def test_embeddings(text: str):
     embeddings_list = embeddings.embed_query(text)
     time_end = time.time()
     return {"embeddings": embeddings_list, "time": time_end - time_start,"model_name":embeddings.model}
+
+@app.get("/test/file_count")
+def test_file_count():
+    directory = "files"  # 設定要檢查的資料夾名稱
+    if os.path.exists(directory):
+        files = os.listdir(directory)  # 獲取資料夾中的所有檔案名稱
+        return {"file_count": len(files), "file_names": files}
+    else:
+        return {"file_count": 0, "message": "No such directory."}
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
--- src/st_web/pages/Embeddings.py
+++ src/st_web/pages/Embeddings.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import requests
+import os
 
 st.header("Embeddings")
+
+# show the embeddings
+
+# show the files that have been uploaded
