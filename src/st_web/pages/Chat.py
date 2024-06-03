7884275ee716b0bdb07f111cf199c6605f650d7c
hibana2077 <hibana2077@gmaill.com>
2024-06-03 17:45:21 +0800

    chore: Update chat input prompt message

--- src/st_web/pages/Chat.py
+++ src/st_web/pages/Chat.py
@@ -35,7 +35,7 @@ def embeddings_search(user_input: str, embedding_name: str) -> dict:
 def init_chat_history() -> ChatPromptTemplate:
     if 'chat_history' not in st.session_state:
         template = ChatPromptTemplate.from_messages([
-            ('system', "You are an AI Teaching Assistant, you need to help students with their questions based on the content of the textbooks."),
+            ('system', "You are an AI Teaching Assistant, you need to help students with their questions based on the content of the textbooks.If your reference content has pages, please provide the page number in your response."),
         ])
         st.session_state['chat_history'] = template
     else:
