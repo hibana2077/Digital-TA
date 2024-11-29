4fe24a8213c26515142e0aca7a4cd7c65d6e8eb1
hibana2077 <hibana2077@gmail.com>
2024-11-29 12:32:24 +0800

    Update default chat model to llama-3.1-70b-versatile-groq in Chat.py

--- src/st_web/pages/Chat.py
+++ src/st_web/pages/Chat.py
@@ -23,7 +23,7 @@ EXTRACT_PROMPT = ChatPromptTemplate.from_template(
 
 if 'chat_model' not in st.session_state:
     # st.session_state['chat_model'] = "llama-3.2-90b-vision-preview-groq"
-    st.session_state['chat_model'] = "llama-3.1-70b-versatile"
+    st.session_state['chat_model'] = "llama-3.1-70b-versatile-groq"
 
 def get_all_embeddings() -> list:
     response = requests.get(f"{BACKEND_SERVER}/embedding_count")
